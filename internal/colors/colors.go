3f9422bc5b250ecc2fa6f620578c1828f7ebe10c
Ari Palo <ari.palo@almamedia.fi>
2022-04-11 17:44:22 +0300

    feat: adjust colors

--- internal/colors/colors.go
+++ internal/colors/colors.go
@@ -4,10 +4,10 @@ import "github.com/gookit/color"
 
 var (
 	Banner  = color.New(color.FgMagenta, color.OpBold)
-	Debug   = color.New(color.Gray)
-	Info    = color.New(color.Gray, color.OpBold)
+	Debug   = color.New(color.FgDarkGray)
+	Info    = color.New(color.FgGray, color.OpBold)
 	Title   = color.New(color.FgLightBlue, color.OpBold)
-	Warning = color.New(color.Yellow, color.OpBold)
+	Warning = color.New(color.Yellow)
 	Failure = color.New(color.FgRed, color.OpBold)
 	Success = color.New(color.FgGreen, color.OpBold)
 	Prompt  = color.New(color.FgCyan, color.OpBold)
