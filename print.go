a0a102c0e375f40405f99e0299e395f081d1dabd
Ari Palo <ari.palo@almamedia.fi>
2022-04-11 14:42:41 +0300

    refactor: refactor & add comments

--- print.go
+++ print.go
@@ -6,6 +6,8 @@ import (
 	"github.com/enescakir/emoji"
 )
 
+// Title prints a heading message in blue color.
+// No-op if Silent Mode active.
 func (m *Message) Title(prefix emoji.Emoji, rendered string) {
 	if m.silentMode {
 		return
@@ -14,6 +16,8 @@ func (m *Message) Title(prefix emoji.Emoji, rendered string) {
 	colors.Title.Println(formatted)
 }
 
+// Debug prints a message in gray for debugging/testing purposes.
+// No-op, unless Verbose Mode active.
 func (m *Message) Debug(prefix emoji.Emoji, rendered string) {
 	if m.verboseMode {
 		formatted := emojiprefix.Format(m.emojiMode, prefix, rendered)
@@ -21,6 +25,8 @@ func (m *Message) Debug(prefix emoji.Emoji, rendered string) {
 	}
 }
 
+// Info prints an informational message in white color.
+// No-op if Silent Mode active.
 func (m *Message) Info(prefix emoji.Emoji, rendered string) {
 	if m.silentMode {
 		return
@@ -29,11 +35,19 @@ func (m *Message) Info(prefix emoji.Emoji, rendered string) {
 	colors.Info.Println(formatted)
 }
 
+// Prompt prints a "prompt query/question" in cyan.
+// Meant to be used for asking user input, just before reading stdin.
+// Printed always, even if Silent Mode active.
+//
+// NOTE: This method only prints the "prompt query/question" and does not
+// actually read the stdin: You need to implement that yourself!
 func (m *Message) Prompt(prefix emoji.Emoji, rendered string) {
 	formatted := emojiprefix.Format(m.emojiMode, prefix, rendered)
 	colors.Prompt.Println(formatted)
 }
 
+// Success prints a message in green.
+// No-op if Silent Mode active.
 func (m *Message) Success(prefix emoji.Emoji, rendered string) {
 	if m.silentMode {
 		return
@@ -42,6 +56,8 @@ func (m *Message) Success(prefix emoji.Emoji, rendered string) {
 	colors.Success.Println(formatted)
 }
 
+// Warning prints a message in yellow.
+// No-op if Silent Mode active.
 func (m *Message) Warning(prefix emoji.Emoji, rendered string) {
 	if m.silentMode {
 		return
@@ -50,6 +66,9 @@ func (m *Message) Warning(prefix emoji.Emoji, rendered string) {
 	colors.Warning.Println(formatted)
 }
 
+// Failure prints a message in red.
+// Usefule for notifying user about errors.
+// Printed always, even if Silent Mode active.
 func (m *Message) Failure(prefix emoji.Emoji, rendered string) {
 	formatted := emojiprefix.Format(m.emojiMode, prefix, rendered)
 	colors.Failure.Println(formatted)
